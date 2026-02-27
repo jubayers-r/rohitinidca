b98177e8312ab2e5995a53d5cfae3d3332af18ff
Jubayer <validjubayer@gmail.com>
2026-02-27 13:02:06 +0600

    remove unnecessary blobs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,21 +7,6 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "ICEPots",
   description: "ICEPots - Not for everyone",
-  openGraph: {
-    images: [
-      {
-        url: "https://bolt.new/static/og_default.png",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    images: [
-      {
-        url: "https://bolt.new/static/og_default.png",
-      },
-    ],
-  },
 };
 
 export default function RootLayout({
